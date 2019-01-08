92ecd3cb33cf6c64e11ec761c73c256a2e961af8
Sabrina Reichel <s0553282@htw-berlin.de>
2019-01-08 16:46:51 +0100

    post methode

--- src/app/board/telefonat/telefonat.component.ts
+++ src/app/board/telefonat/telefonat.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 
 @Component({
@@ -7,10 +9,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./telefonat.component.css'],
 })
 export class TelefonatComponent implements OnInit {
-  constructor() { }
 
-  ngOnInit() {
+  constructor(private http: HttpClient) {}
+
+  urlServer:string = 'http://localhost:8080/telefonat';
+  @Input('ngModel')
+  caller:string = '';
+  @Input('ngModel')
+  phonenumber:string = '';
+  @Input('ngModel')
+  message:string = '';
+
+  //params = new HttpParams();
+  map = new Map();
+
+  ngOnInit(){}
+
+  onSubmit() {
+    // this.params.set('caller', caller)
+    //    .set('phonenumber', this.phonenumber)
+    //    .set('message', this.message);
+
+    const headers = new HttpHeaders()
+      .set("X-CustomHeader", "custom header value");
+
+    this.map.set('caller', this.caller)
+      .set('phonenumber', this.phonenumber)
+      .set('message', this.message);
+    console.log(this.caller);
+    let urlNew = this.urlServer + '/new'
+    console.log(urlNew);
+    let req = this.http.post(urlNew, this.map).subscribe(
+      res => {
+        console.log(res);
+      },
+      err => {
+        console.log("Error occured");
+      });
+
+
+    // let req = this.http.post(this.urlServer + '/new', data)
+    //   .subscribe(
+    //   res => {
+    //   console.log(res);
+    // },
+    // err => {
+    //   console.log("Error occured");
+    // });
+
   }
-}
 
+}
 
