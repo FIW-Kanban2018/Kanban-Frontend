bc7cfae9eac4ec046dbca5b7dae80b4578c0e90e
julia-as <julia.e.as@gmail.com>
2019-01-17 15:31:35 +0100

    working on post method

--- src/app/board/telefonat/telefonat.component.ts
+++ src/app/board/telefonat/telefonat.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {MatSnackBar} from '@angular/material';
+import {MatRadioChange, MatSnackBar} from '@angular/material';
 
 
 @Component({
@@ -14,15 +14,11 @@ export class TelefonatComponent implements OnInit {
 
   constructor(private http: HttpClient, public snackBarRef: MatSnackBar) {}
 
-  urlServer:string = 'http://localhost:8080/telefonat';
-  @Input('ngModel')
-  caller:string = '';
-  @Input('ngModel')
-  phonenumber:string = '';
-  @Input('ngModel')
-  message:string = '';
+  caller = '';
+  phoneNumber = '';
+  message = '';
+  category = '';
 
-  //params = new HttpParams();
   map = new Map();
 
   ngOnInit(){}
@@ -32,34 +28,68 @@ export class TelefonatComponent implements OnInit {
     //    .set('phonenumber', this.phonenumber)
     //    .set('message', this.message);
 
-    const headers = new HttpHeaders()
-      .set("X-CustomHeader", "custom header value");
+    // const headers = new HttpHeaders()
+    //   .set("X-CustomHeader", "custom header value");
+    //
+    // this.map.set('caller', this.caller)
+    //   .set('phonenumber', this.phonenumber)
+    //   .set('message', this.message);
+    // console.log(this.caller);
+    // let urlNew = this.urlServer + '/new'
+    // console.log(urlNew);
+    // let req = this.http.post(urlNew, this.map).subscribe(
+    //   res => {
+    //     console.log(res);
+    //   },
+    //   err => {
+    //     console.log("Error occured");
+    //   });
 
-    this.map.set('caller', this.caller)
-      .set('phonenumber', this.phonenumber)
-      .set('message', this.message);
-    console.log(this.caller);
-    let urlNew = this.urlServer + '/new'
-    console.log(urlNew);
-    let req = this.http.post(urlNew, this.map).subscribe(
+
+    this.telefonatService.sendData(this.map).subscribe(
       res => {
         console.log(res);
       },
       err => {
         console.log("Error occured");
       });
+
     this.snackBarRef.open('Karte wurde angelegt!' , 'Oki Doki', {duration: 1500});
+  }
 
+  setData(){
+    this.map.set('caller', this.caller)
+      .set('message', this.message)
+      .set('phonenumber', this.phoneNumber)
+      .set('category', this.category);
 
-    // let req = this.http.post(this.urlServer + '/new', data)
-    //   .subscribe(
-    //   res => {
-    //   console.log(res);
-    // },
-    // err => {
-    //   console.log("Error occured");
-    // });
+    console.log('caller: ' + this.caller);
+    console.log('phone: ' + this.phoneNumber);
+    console.log('message: ' + this.message);
+    console.log('category: ' + this.category);
+  }
 
+  onUpdateCaller(event: Event) {
+    this.caller = (<HTMLInputElement>event.target).value;
+  }
+
+  onUpdatePhone(event: Event) {
+    this.phoneNumber = (<HTMLInputElement>event.target).value;
+  }
+
+  onUpdateMessage(event: Event) {
+    this.message = (<HTMLInputElement>event.target).value;
+  }
+
+  changeRadioValue(event: MatRadioChange){
+    //this.category = this.selection;
+    // this.category = mrChange.value;
+    console.log('event.value: ' + event.value);
+    //console.log('category via selection: ' + this.category);
+    this.category = event.value;
+    console.log('category via event.value' + this.category);
+    console.log('event.source.name' + event.source.name);
+  }
   }
 
 }
